ffe2f8a0c87a2ab19bd72db0c8521c766f884ce3
Matheesha <matheesha.j@botcalm.com>
2025-06-01 21:00:19 +0530

    Creating browser fixture

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# // This fixture sets up a Selenium WebDriver instance for testing
+# def driver():
+#     driver = webdriver.Chrome()  # You can change this to any other browser driver
+#     driver.get("http://example.com")  # Replace with the URL you want to test
+#     yield driver
+#     driver.quit()
+
+@pytest.fixture(scope="class")
+def browser():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    yield driver
+    driver.quit()
