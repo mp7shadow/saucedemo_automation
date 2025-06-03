f46f5122fac062a27d01ce1e59270a13bc535824
Matheesha <matheesha.j@botcalm.com>
2025-06-03 23:05:04 +0530

    Rename folders

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -2,20 +2,29 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-from base_pages.Login_Page import Login_Page
+from configurations.config import Config	
+from pages.Login_Page import Login_Page
 
 
 class Test_01_Login:
 
-    login_page_url = "https://www.saucedemo.com/"
-    username = "standard_user"
-    password = "secret_sauce"
+    # Class variables for the login page URL and credentials
+
+    login_page_url = Config.base_url	
+
+    # Valid credentials for testing successful login
+    username = Config.valid_username
+    password = Config.valid_password
+
+    # Invalid credentials for testing error scenarios
     invalid_username = "asdfdf"
     invalid_password = "1112"
     empty_username = ""
     empty_password =""
 
+    # Test case to verify the title of the login page
     def test_title_verification(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -28,7 +37,8 @@ class Test_01_Login:
             self.driver.close()
             assert False
 
-
+# Test cases for login functionality
+    # Test case to verify valid login
     def test_valid_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -44,6 +54,7 @@ class Test_01_Login:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Empty Username
     def test_empty_username_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -51,14 +62,20 @@ class Test_01_Login:
         self.login_page.enter_username(self.empty_username)
         self.login_page.enter_password(self.password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
-        if err_message == "Epic sadface: Username and password do not match any user in this service":
+        err_element = EC.presence_of_element_located((By.CSS_SELECTOR, "[data-test='error']"))
+        err_message = EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='error']"))
+        WebDriverWait(self.driver, 10).until(err_element)
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
+        print(err_message)
+        # Check if the error message matches the expected message for empty username    
+        if err_message == "Epic sadface: Username is required":
             assert True
             self.driver.close()
         else:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Empty Password
     def test_empty_password_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -66,14 +83,15 @@ class Test_01_Login:
         self.login_page.enter_username(self.username)
         self.login_page.enter_password(self.empty_password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
-        if err_message == "Epic sadface: Username and password do not match any user in this service":
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
+        if err_message == "Epic sadface: Password is required":
             assert True
             self.driver.close()
         else:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Empty Login
     def test_empty_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -81,14 +99,15 @@ class Test_01_Login:
         self.login_page.enter_username(self.empty_username)
         self.login_page.enter_password(self.empty_password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
-        if err_message == "Epic sadface: Username and password do not match any user in this service":
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
+        if err_message == "Epic sadface: Username is required":
             assert True
             self.driver.close()
         else:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Invalid Login
     def test_invalid_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -96,7 +115,7 @@ class Test_01_Login:
         self.login_page.enter_username(self.invalid_username)
         self.login_page.enter_password(self.invalid_password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
         if err_message == "Epic sadface: Username and password do not match any user in this service":
             assert True
             self.driver.close()
@@ -104,6 +123,7 @@ class Test_01_Login:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Invalid Username
     def test_invalid_username_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -111,7 +131,7 @@ class Test_01_Login:
         self.login_page.enter_username(self.invalid_username)
         self.login_page.enter_password(self.password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
         if err_message == "Epic sadface: Username and password do not match any user in this service":
             assert True
             self.driver.close()
@@ -119,6 +139,7 @@ class Test_01_Login:
             self.driver.close()
             assert False
 
+    # Test cases for invalid login scenarios - Invalid Password
     def test_invalid_password_login(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.login_page_url)
@@ -126,7 +147,7 @@ class Test_01_Login:
         self.login_page.enter_username(self.username)
         self.login_page.enter_password(self.invalid_password)
         self.login_page.click_login_button()
-        err_message = self.driver.find_element(By.XPATH,"//div[@class='error-message-container error']//h3//button").text
+        err_message = self.driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
         if err_message == "Epic sadface: Username and password do not match any user in this service":
             assert True
             self.driver.close()
