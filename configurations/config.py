d0a28918194988127bf6756a61213d68636835b5
Matheesha <matheesha.j@botcalm.com>
2025-06-03 23:05:40 +0530

    Update config

---
+++ configurations/config.py
@@ -0,0 +1,10 @@
+
+class Config:
+
+
+    # Base URL for the application
+    base_url = "https://www.saucedemo.com/"
+    
+    # Valid credentials for login
+    valid_username = "standard_user"
+    valid_password = "secret_sauce" 
