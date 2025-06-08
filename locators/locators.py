3afbb31ac04ec30c714780621240b1f28cdf7e0c
Matheesha <matheesha.j@botcalm.com>
2025-06-09 02:53:59 +0530

    Update locators.py

--- locators/locators.py
+++ locators/locators.py
@@ -8,12 +8,13 @@ class LoginLocators:
     # Error message locator
     # This locator is used to find the error message displayed on the login page
     error_message = (By.XPATH, "//h3[@data-test='error']")
-    # Error messages for the login page
-    error_message = "Epic sadface: Username and password do not match any user in this service"
-    error_message_empty_username = "Epic sadface: Username is required"
-    error_message_empty_password = "Epic sadface: Password is required"
-    error_message_empty_login = "Epic sadface: Username is required"
-    error_message_invalid_login = "Epic sadface: Username and password do not match any user in this service"
+   
+    # # Error messages for the login page
+    # error_message = "Epic sadface: Username and password do not match any user in this service"
+    # error_message_empty_username = "Epic sadface: Username is required"
+    # error_message_empty_password = "Epic sadface: Password is required"
+    # error_message_empty_login = "Epic sadface: Username is required"
+    # error_message_invalid_login = "Epic sadface: Username and password do not match any user in this service"
 
 class InventoryLocators:
 
