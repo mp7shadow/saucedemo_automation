b0f5bd30f42fcfcc5047eb8167ba0b4252166a13
Matheesha <matheesha.j@botcalm.com>
2025-06-09 01:37:00 +0530

    Add combined login method

--- locators/locators.py
+++ locators/locators.py
@@ -4,7 +4,21 @@ class LoginLocators:
     username_input = (By.ID, "user-name")
     password_input = (By.ID, "password")
     login_btn = (By.ID, "login-button")
-    error_message = (By.CSS_SELECTOR, "h3[data-test='Epic sadface']")
 
-# class InventoryLocators:
-#     INVENTORY_CONTAINER = (By.ID, "inventory_container")
+    # Error message locator
+    # This locator is used to find the error message displayed on the login page
+    error_message = (By.XPATH, "//h3[@data-test='error']")
+    # Error messages for the login page
+    error_message = "Epic sadface: Username and password do not match any user in this service"
+    error_message_empty_username = "Epic sadface: Username is required"
+    error_message_empty_password = "Epic sadface: Password is required"
+    error_message_empty_login = "Epic sadface: Username is required"
+    error_message_invalid_login = "Epic sadface: Username and password do not match any user in this service"
+
+class InventoryLocators:
+
+    # Locators for the inventory page
+    # These locators are used to find elements on the inventory page
+    
+    INVENTORY_CONTAINER = (By.ID, "inventory_container")
+
--- pages/Login_Page.py
+++ pages/Login_Page.py
@@ -1,10 +1,13 @@
 from selenium.webdriver.common.by import By
 from locators.locators import LoginLocators
+from configurations.config import Config
 
 
 class Login_Page:
 
     # locators
+    # these are the locators for the login page elements
+    # they are used to find the elements on the page
     textbox_username_id = LoginLocators.username_input[1]
     textbox_password_id = LoginLocators.password_input[1]   
     btn_login_id = LoginLocators.login_btn[1]
@@ -13,19 +16,34 @@ class Login_Page:
     def __init__(self,driver):
         self.driver = driver
 
-    # actions
+    # action methods
+    # these methods are used to perform actions on the login page
+
+    # this method is used to enter the username in the username textbox
     def enter_username(self,username):
 
         self.driver.find_element(By.ID,self.textbox_username_id).clear()
         self.driver.find_element(By.ID,self.textbox_username_id).send_keys(username)
 
+    # this method is used to enter the password in the password textbox
     def enter_password(self,password):
 
         self.driver.find_element(By.ID,self.textbox_password_id).clear()
         self.driver.find_element(By.ID,self.textbox_password_id).send_keys(password)
 
+    # this method is used to click on the login button
     def click_login_button(self):
         self.driver.find_element(By.ID,self.btn_login_id).click()
+
+    # combined action for login
+    # this method can be used to login with username and password
+    def login(self, username = Config.valid_username, password = Config.valid_password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login_button()
+
+
+
 
 # from locators.locators import LoginLocators
 # from pages.Base_Page import BasePage
