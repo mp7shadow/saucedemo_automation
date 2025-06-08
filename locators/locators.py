aa4e4e8431c7033cf66937962a6e75ebaffdbe12
Matheesha <matheesha.j@botcalm.com>
2025-06-09 02:18:22 +0530

    Update locators.py

--- locators/locators.py
+++ locators/locators.py
@@ -1,10 +1,13 @@
 from selenium.webdriver.common.by import By
 
 class LoginLocators:
+    # Locators for the login page
     username_input = (By.ID, "user-name")
     password_input = (By.ID, "password")
     login_btn = (By.ID, "login-button")
     error_message = (By.CSS_SELECTOR, "h3[data-test='Epic sadface']")
 
-# class InventoryLocators:
-#     INVENTORY_CONTAINER = (By.ID, "inventory_container")
+
+class InventoryLocators:
+    # Locators for the inventory page
+    INVENTORY_CONTAINER = (By.ID, "inventory_container")
