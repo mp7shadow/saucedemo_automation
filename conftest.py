bbaa8fef7f0daa3d537c958ba53c3d5a60caf7cc
Matheesha <matheesha.j@botcalm.com>
2025-06-10 18:54:33 +0530

    Change fixture scope to "function"

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def driver():
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
