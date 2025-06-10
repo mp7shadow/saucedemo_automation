5fc1dd2da024f2d09e3fdd96e4ff0bc6b0cec425
Matheesha <matheesha.j@botcalm.com>
2025-06-11 00:34:33 +0530

    Fix test_sidemenu errors

--- test_cases/test_sidemenu.py
+++ test_cases/test_sidemenu.py
@@ -2,7 +2,8 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from locators.locators import InventoryLocators, MenuLocators
+from locators.locators import InventoryLocators, MenuLocators, ShoppingCartLocators
+from pages.Cart_Page import Cart_Page
 from pages.Inventory_Page import Inventory_Page
 from pages.Login_Page import Login_Page
 from pages.Menu_Page import Menu_Page
@@ -13,6 +14,7 @@ from configurations.config import Config
 class Test_03_Sidebar_Menu:
 
     base_url = Config.base_url
+
 
     def test_open_sidebar_menu(self, driver):
         try:
@@ -54,18 +56,26 @@ class Test_03_Sidebar_Menu:
             # Verify that the inventory page is loaded
             assert inventory_page.is_inventory_page_loaded() == True
 
-            # Navigate to cart page to ensure the sidebar menu "All Items" link is functioning correctly
-            
-
-            # Open the sidebar menu
+            # First, navigate to shopping cart to ensure the sidebar menu "All Items" link is functioning correctly
+            cart = Cart_Page(driver)
+            cart.go_to_cart()
+            # Verify that the cart page is loaded
+            header_text = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(ShoppingCartLocators.cart_page_header)
+            ).text
+            assert header_text == "Your Cart", "Shopping cart page did not load correctly."
+            # Now, open the sidebar menu
             menu = Menu_Page(driver)
             menu.open_menu()
 
             # Click on 'All Items' link
             menu.click_all_items()
-
+            # Wait for the inventory page to load
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(By.XPATH, InventoryLocators.header_title)
+            )
             # Verify that the inventory page is displayed
-            act_inventory = driver.find_element(By.XPATH, InventoryLocators.header_title).text
+            act_inventory = driver.find_element(InventoryLocators.header_title).text
             assert "Products" in act_inventory, "'All Items' link did not navigate to the Products page."
 
             print("TS03_TC02 - 'All Items' link verified successfully.")
@@ -75,6 +85,62 @@ class Test_03_Sidebar_Menu:
         finally:
             driver.quit()
 
+    def test_sidebar_menu_about(self, driver):
+        try:
+            inventory_page = Inventory_Page(driver)
+            # Load the inventory page
+            inventory_page.load_inventory_page()
+            # Verify that the inventory page is loaded
+            assert inventory_page.is_inventory_page_loaded() == True
+
+            # Open the sidebar menu
+            menu = Menu_Page(driver)
+            menu.open_menu()
+
+            # Click on 'About' link
+            menu.click_about()
+
+            # Wait for the URL to change to Sauce Labs landing page
+            WebDriverWait(driver, 10).until(
+                EC.url_contains("saucelabs.com")
+            )
+            assert "saucelabs.com" in driver.current_url, "'About' link did not navigate to Sauce Labs landing page."
+
+            print("TS03_TC03 - 'About' link navigates to Sauce Labs Landing page successfully.")
+        except Exception as e:
+            print(f"TS03_TC03 - Failed to verify 'About' link: {e}")
+            assert False
+        finally:
+            driver.quit()
+
+    def test_sidebar_menu_logout(self, driver):
+        try:
+            inventory_page = Inventory_Page(driver)
+            # Load the inventory page
+            inventory_page.load_inventory_page()
+            # Verify that the inventory page is loaded
+            assert inventory_page.is_inventory_page_loaded() == True
+
+            # Open the sidebar menu
+            menu = Menu_Page(driver)
+            menu.open_menu()
+
+            # Click on 'Logout' link
+            menu.click_logout()
+
+            # Wait for the login button to be present
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(Login_Page.login_button_id)
+            )
+            login_button = driver.find_element(Login_Page.login_button_id).text
+            assert "login" in login_button, "'Logout' did not navigate to the login page."
+
+            print("TS03_TC04 - Logout successful.")
+        except Exception as e:
+            print(f"TS03_TC04 - Failed to verify 'Logout' link: {e}")
+            assert False
+        finally:
+            driver.quit()
 
     # def login(self, driver):
     #     driver.get(self.base_url)
