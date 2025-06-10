8474bee87bec8d7d3b9f1edcd9cefe5e589cb12e
Matheesha <matheesha.j@botcalm.com>
2025-06-11 00:32:46 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,6 +6,10 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-save-password-bubble")
     chrome_options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
