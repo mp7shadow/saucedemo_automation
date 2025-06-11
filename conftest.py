2cb48db0d44cba84065b9c5f9c154294f25c7d59
Matheesha <matheesha.j@botcalm.com>
2025-06-11 09:08:06 +0530

    Add incognito option to browser

--- conftest.py
+++ conftest.py
@@ -6,15 +6,22 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
-    chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-save-password-bubble")
     chrome_options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
-        "profile.password_manager_enabled": False
+        "profile.password_manager_enabled": False,
+        "profile.default_content_setting_values.notifications": 2,
+        "profile.default_content_setting_values.geolocation": 2
     })
+    chrome_options.add_argument("--incognito")
 
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
