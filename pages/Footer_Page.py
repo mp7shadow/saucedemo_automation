d63cc2bcc33c820f47141d198fa58b9d3cbbd024
Matheesha <matheesha.j@botcalm.com>
2025-06-11 09:09:48 +0530

    Create Footer_Page.py

---
+++ pages/Footer_Page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators.locators import FooterLocators
+
+class Footer_Page:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def is_footer_visible(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(FooterLocators.footer_container)
+        )
+
+    def get_footer_text(self):
+        return self.driver.find_element(*FooterLocators.footer_container).text
+
+    def is_twitter_link_present(self):
+        return self.driver.find_element(*FooterLocators.footer_twitter_link).is_displayed()
+
+    def is_facebook_link_present(self):
+        return self.driver.find_element(*FooterLocators.footer_facebook_link).is_displayed()
+
+    def is_linkedin_link_present(self):
+        return self.driver.find_element(*FooterLocators.footer_linkdin_link).is_displayed()
