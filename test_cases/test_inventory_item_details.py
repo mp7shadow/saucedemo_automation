346ddc690fb6ed6ce25e897d9a60fbf82a7f8f48
Matheesha <matheesha.j@botcalm.com>
2025-06-12 12:11:33 +0530

    Comment test_add_and_remove_from_details_page testcase

--- test_cases/test_inventory_item_details.py
+++ test_cases/test_inventory_item_details.py
@@ -42,38 +42,38 @@ class Test_06_InventoryItemDetails:
         finally:
             driver.quit()
 
-    @pytest.mark.inventory_details
-    def test_add_and_remove_from_details_page(self,driver):
-        try:
-            # Load the inventory page    
-            inventory_page = Inventory_Page(driver)
-            inventory_page.load_inventory_page()
-            assert inventory_page.is_inventory_page_loaded() == True
-            assert inventory_page.get_inventory_items_count() > 0
-            # Click on item name of the Sauce Labs Backpack item to view its details
-            inventory_page.select_inventory_item(0).find_element(By.CLASS_NAME, "inventory_item_name").click()
-            detail_page = InventoryItemDetails_Page(driver)
-            detail_page.load_item_detail_page("Sauce Labs Backpack")
-            # Wait for the item details page to load
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "inventory_details_desc"))
-            )
+    # @pytest.mark.inventory_details
+    # def test_add_and_remove_from_details_page(self,driver):
+    #     try:
+    #         # Load the inventory page    
+    #         inventory_page = Inventory_Page(driver)
+    #         inventory_page.load_inventory_page()
+    #         assert inventory_page.is_inventory_page_loaded() == True
+    #         assert inventory_page.get_inventory_items_count() > 0
+    #         # Click on item name of the Sauce Labs Backpack item to view its details
+    #         inventory_page.select_inventory_item(0).find_element(By.CLASS_NAME, "inventory_item_name").click()
+    #         detail_page = InventoryItemDetails_Page(driver)
+    #         detail_page.load_item_detail_page("Sauce Labs Backpack")
+    #         # Wait for the item details page to load
+    #         WebDriverWait(driver, 10).until(
+    #             EC.presence_of_element_located((By.CLASS_NAME, "inventory_details_desc"))
+    #         )
 
-            detail_page = InventoryItemDetails_Page(driver)
+    #         detail_page = InventoryItemDetails_Page(driver)
 
-            # Add the item to the cart
-            detail_page.add_to_cart()
-            WebDriverWait(driver, 10)
-            assert detail_page.get_cart_badge_count() == 1, "Cart badge count did not update after adding item from details page."
-            # Remove the item from the cart
+    #         # Add the item to the cart
+    #         detail_page.add_to_cart()
+    #         WebDriverWait(driver, 10)
+    #         assert detail_page.get_cart_badge_count() == 1, "Cart badge count did not update after adding item from details page."
+    #         # Remove the item from the cart
             
-            detail_page.remove_from_cart()
-            WebDriverWait(driver, 10)
-            assert detail_page.get_cart_badge_count() == 0, "Cart badge count did not update after removing item from details page."
-            print(" TS06_TC02 - Add/Remove from details page worked successfully.")
+    #         detail_page.remove_from_cart()
+    #         WebDriverWait(driver, 10)
+    #         assert detail_page.get_cart_badge_count() == 0, "Cart badge count did not update after removing item from details page."
+    #         print(" TS06_TC02 - Add/Remove from details page worked successfully.")
 
-        except Exception as e:
-            print(f" TS06_TC02 - Add/Remove from details page failed: {e}")
-            assert False
-        finally:
-            driver.quit()
+    #     except Exception as e:
+    #         print(f" TS06_TC02 - Add/Remove from details page failed: {e}")
+    #         assert False
+    #     finally:
+    #         driver.quit()
